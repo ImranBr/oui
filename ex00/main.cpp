eb114a43256ee3001eb164ae292088d135aa67d3
Imran Barbouch <imranbarbouch@hotmail.fr>
2026-02-24 22:44:16 +0100

    00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,15 +1,37 @@
+#include "Animal.hpp"
 #include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
+    std::cout << "=== Test via Animal* ===" << std::endl;
+    const Animal* meta = new Animal();
+    std::cout << "Type: " << meta->getType() << std::endl;
+    meta->makeSound();
+    delete meta;
+    std::cout << std::endl;
+    
     std::cout << "=== Test Dog via Animal* ===" << std::endl;
+    const Animal* d = new Dog();
+    std::cout << "Type: " << d->getType() << std::endl;
+    d->makeSound();    
+    delete d;
+    std::cout << std::endl;
 
-    const Animal* a = new Dog();
+    std::cout << "=== Test Cat via Animal* ===" << std::endl;
+    const Animal* c = new Cat();
+    std::cout << "Type: " << c->getType() << std::endl;
+    c->makeSound();
+    delete c;
+    std::cout << std::endl;
 
-    std::cout << "Type: " << a->getType() << std::endl;
-    a->makeSound();
-
-    delete a;
+    std::cout << "=== Test Cat via WrongAnimal* ===" << std::endl;
+    const WrongAnimal* w = new WrongCat();
+    std::cout << "Type: " << w->getType() << std::endl;
+    w->makeSound();
+    delete w;
 
     return 0;
 }
